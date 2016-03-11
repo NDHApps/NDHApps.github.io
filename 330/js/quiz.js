b148314ec0a0d49e953af25cb41acb830aff9322
NDHApps <nhall7@comcast.net>
2016-03-11 01:42:09 -0600

    Updated candidate data

--- 330/js/quiz.js
+++ 330/js/quiz.js
@@ -1,9 +1,29 @@
+var candidates; 
+
 $(document).ready(function() {
     loadJSON('http://ndhapps.com/330/js/data/candidates.json',function(data) {
-        var candidates = JSON.parse(data).candidates;
+        candidates = JSON.parse(data).candidates;
         console.log(candidates);
     });
+    
+    $('.question input[type=range]').on('input', function() {
+        sessionStorage.setItem($(this).attr('id'), $(this).val());
+    });
 });
+
+function loadJSON(path, callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
 
 function loadJSON(path, callback) {
 
